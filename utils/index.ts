b9aa37cbfa62107953051f8b9dacda39013872af
reyu <ka15sugar@gmail.com>
2021-03-16 04:09:50 +0900

    fix: getcontent

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,7 @@
 const isBot = (res: HubotTraq.Response) => res.message.message.user.bot
 
 const getContent = (res: HubotTraq.Response) => {
-  const re = new RegExp('@[a-zA-Z0-9_-]+\\s*(.*)')
+  const re = new RegExp('@[a-zA-Z0-9_-]+\\s*[^\\s]*\\s*(.*)')
   const { plainText } = res.message.message
   const result = plainText.match(re)
   return !result ? '' : result[1]
