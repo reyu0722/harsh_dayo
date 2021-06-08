05f6e486dc047b84b63ea4e900009d6a2df77270
reyu <ka15sugar@gmail.com>
2021-06-09 03:18:51 +0900

    fix: fetch atcoder contest

--- scripts/contest.ts
+++ scripts/contest.ts
@@ -7,9 +7,17 @@ import { exec, makeTable } from '../utils'
 import fetch from 'node-fetch'
 import { parse } from 'node-html-parser'
 
-const getContests = async () => {
+type Contest = {
+  name: string
+  url: string
+  startTime: string
+  duration: string
+}
+
+
+const getAtCoderContests = async (): Promise<Contest[]> => {
   const response = await fetch('https://atcoder.jp/contests/')
-  if (!response.ok) throw new Error('コンテスト情報の取得に失敗しました')
+  if (!response.ok) throw new Error('failed to fetch AtCoder contests')
   const body = await response.text()
 
   const doc = parse(body)
@@ -17,8 +25,23 @@ const getContests = async () => {
   const raws = table.querySelectorAll('tr')
 
   return raws.slice(1).map(raw => {
-    const aTag = raw.querySelectorAll('a')[1].toString()
-    const timeTag = raw.querySelector('time').toString()
+    const td = raw.querySelectorAll('td')
+    const timeTag = td[0].toString()
+    const nameTag = td[1].querySelector('a').toString()
+    const durationTag = td[2].toString()
+
+    const name = (nameTag.match(/<.*?>([^<]*?)<\/.*?>/) ?? [])[1]
+    const url = (nameTag.match(/<a href="(.*?)".*/) ?? [])[1]
+
+    const timeStr = (timeTag.match(/\?iso=(.*?)&/) ?? [])[1]
+    const startTime = `${timeStr.substr(4, 2)}/${timeStr.substr(6, 2)} ${timeStr.substr(9, 2)}:${timeStr.substr(11, 2)}`
+
+    const [_, min, sec] = durationTag.match(/>([^<]*?):([^<]*?)<\//) ?? []
+    // const duration = (parseInt(min) * 60 + parseInt(sec)) * 1000
+    const duration = `${min}:${sec}`
+    return { name, url, startTime, duration }
+  })
+}
 
     const name = (aTag.match(/<.*?>(.*?)<\/.*?>/) ?? [])[1]
     const link = (aTag.match(/<a href="(.*?)".*/) ?? [])[1]
@@ -33,7 +56,7 @@ const getContests = async () => {
 module.exports = (robot: HubotTraq.Robot) => {
   robot.respond(/contest/i, res =>
     exec(res, async () => {
-      const contests = makeTable(await getContests())
+      const contests = makeTable(await getAtCoderContests())
       if (contests == '') res.send('コンテストが見つかりませんでした')
       else res.send(contests)
     })
